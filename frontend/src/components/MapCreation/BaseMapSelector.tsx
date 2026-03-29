bf22c3f433c7dcbfc8345eb778e6256ee207aec7
OECM System <rakuri2026@gmail.com>
2026-03-29 21:23:48 +0545

    UI: Set satellite imagery as default map layer

--- frontend/src/components/MapCreation/BaseMapSelector.tsx
+++ frontend/src/components/MapCreation/BaseMapSelector.tsx
@@ -11,17 +11,17 @@ export const BaseMapSelector: React.FC = () => {
   return (
     <>
       <LayersControl position="topright">
-        <BaseLayer checked name="Street Map">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            maxZoom={19}
-          />
-        </BaseLayer>
-        <BaseLayer name="Satellite Imagery">
+        <BaseLayer name="Satellite Imagery" checked>
           <TileLayer
             attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            maxZoom={19}
+          />
+        </BaseLayer>
+        <BaseLayer name="Street Map">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             maxZoom={19}
           />
         </BaseLayer>
